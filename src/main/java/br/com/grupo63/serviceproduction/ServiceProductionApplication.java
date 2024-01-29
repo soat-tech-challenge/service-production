6b844adf4b8d739e5d2fbfaa831913e225ad6f58
g-otn <44736064+g-otn@users.noreply.github.com>
2024-01-29 12:20:18 -0300

    fix: prepare cloud

--- src/main/java/br/com/grupo63/serviceproduction/ServiceProductionApplication.java
+++ src/main/java/br/com/grupo63/serviceproduction/ServiceProductionApplication.java
@@ -12,9 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @OpenAPIDefinition(
         info = @Info(title = "${info.name}", description = "${info.description}", version = "${info.version}"),
         servers ={
-                @Server(url = "${server.servlet.context-path}", description = "Current URL"),
-                @Server(url = "localhost:8080", description = "Local"),
-                @Server(url = "${docs.api.url}", description = "API Gateway Invoke URL")
+                @Server(url = "${server.servlet.context-path:}", description = "Current URL"),
+                @Server(url = "localhost:${server.port:8080}${server.servlet.context-path:}", description = "Localhost"),
+                @Server(url = "${app.docs-api-url:(no value)}${server.servlet.context-path:}", description = "Custom URL from env")
         })
 @SecurityScheme(
         name = "bearerAuth",
